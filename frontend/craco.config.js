f2b6635d91f235894bcb2fd41da2a28e0834a7a5
TAEHONG-LEE <2t_hong@naver.com>
2024-06-28 22:46:25 +0900

    feat: add emotion style library and adapt reset css

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,3 +1,4 @@
+// craco.config.js
 const path = require('path');
 
 module.exports = {
@@ -7,6 +8,7 @@ module.exports = {
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@stories': path.resolve(__dirname, 'src/stories'),
+      '@styles': path.resolve(__dirname, 'src/styles'),  // 새로운 styles 경로 추가
     },
   },
 };
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 // src/App.tsx
 import React from 'react';
-import '@reset';
+import { Global } from '@emotion/react';
+import globalStyles from '@styles/globalStyles'; // 글로벌 스타일 import
+import logo from './logo.svg';
 import './App.css';
 import { Button } from '@stories/Button';
 import { PrimaryArgs } from '@stories/Button.stories';
@@ -8,6 +10,7 @@ import { PrimaryArgs } from '@stories/Button.stories';
 function App() {
     return (
         <div className="App">
+            <Global styles={globalStyles} />
             <Button {...PrimaryArgs} />
         </div>
     );
